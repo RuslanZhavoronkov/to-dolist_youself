f920cadad1845d29b595888b61934255c671da72
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-11-22 16:04:52 +0300

    Lesson 17 - start

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,131 +1,89 @@
-import React from "react";
-import Grid from "@mui/material/Grid";
-import Checkbox from "@mui/material/Checkbox";
-import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
-import FormLabel from "@mui/material/FormLabel";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import { useFormik } from "formik";
-import { useAppDispatch, useAppSelector } from "../../app/store";
-import { loginTC } from "./auth-reducer";
-import { Navigate } from "react-router-dom";
-
-type FormikErrorType = {
-  email?: string;
-  password?: string;
-  //rememberMe?: boolean
-};
-
-export type LoginDataType = {
-email:string
-password: string
-rememberMe: boolean
-}
+import React from 'react'
+import { useFormik } from 'formik'
+import { useSelector } from 'react-redux'
+import { loginTC } from './auth-reducer'
+import { AppRootStateType } from '../../app/store'
+import { Navigate } from 'react-router-dom'
+import { useAppDispatch } from '../../hooks/useAppDispatch';
+import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from '@mui/material'
 
 export const Login = () => {
-  const dispatch = useAppDispatch()
-  const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
+    const dispatch = useAppDispatch()
+
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn);
+
+    const formik = useFormik({
+        validate: (values) => {
+            if (!values.email) {
+                return {
+                    email: 'Email is required'
+                }
+            }
+            if (!values.password) {
+                return {
+                    password: 'Password is required'
+                }
+            }
+
+        },
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        onSubmit: values => {
+            dispatch(loginTC(values));
+        },
+    })
+
+    if (isLoggedIn) {
+        return <Navigate to={"/"} />
+    }
 
 
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-      rememberMe: false,
-    },
-    validate: (values) => {
-      const errors: FormikErrorType = {};
-      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-      if (!values.email) {
-        errors.email = "Required";
-      } else if (!regex.test(values.email)) {
-        errors.email = "Invalid email address";
-      }
-      if (!values.password) {
-        errors.password = "Required";
-      } else if(values.password.length < 5) {
-        errors.password = "Should be more five symbols"
-      }
-      return errors;
-    },
-    onSubmit: (values) => {
-     dispatch(loginTC(values))
-     console.log(values)
-      formik.resetForm();
-    },
-  });
-  //const response = Object.values(formik.errors)
-  //console.log(formik.touched)
-
-  if (isLoggedIn) {
-   return  <Navigate to={'/'}/>
-  }
-  return (
-    <Grid container justifyContent={"center"}>
-      <Grid item justifyContent={"center"}>
-        <FormControl>
-          <FormLabel>
-            <p>
-              To log in get registered
-              <a
-                href={"https://social-network.samuraijs.com/"}
-                target={"_blank"}
-              >
-                {" "}
-                here
-              </a>
-            </p>
-            <p>or use common test account credentials:</p>
-            <p>Email: free@samuraijs.com</p>
-            <p>Password: free</p>
-          </FormLabel>
-          <form onSubmit={formik.handleSubmit}>
-            <FormGroup>
-              <TextField
-                label="Email"
-                margin="normal"
-                {...formik.getFieldProps('email')}
-                // name="email"
-                // onChange={formik.handleChange} //change data form
-                // value={formik.values.email}
-                // onBlur={formik.handleBlur} //visited fields
-              />
-              {formik.touched.email && formik.errors.email && (
-                <span style={{ color: "red" }}>{formik.errors.email}</span>
-              )}
-              <TextField
-                type="password"
-                label="Password"
-                margin="normal"
-                {...formik.getFieldProps('password')}
-                // name="password"
-                // onChange={formik.handleChange} //change data form
-                // value={formik.values.password}
-                // onBlur={formik.handleBlur} ////visited fields    
-              />
-              {formik.touched.password && formik.errors.password && (
-                <span style={{ color: "red" }}>{formik.errors.password}</span>
-              )}
-              <FormControlLabel
-                label={"Remember me"}
-                control={
-                  <Checkbox
-                    {...formik.getFieldProps('rememberMe')}
-                    // name={"rememberMe"}
-                    // onChange={formik.handleChange}  
-                  />
-                } //change data form
-                value={formik.values.rememberMe}
-              />
-              <Button type={"submit"} variant={"contained"} color={"primary"}>
-                Login
-              </Button>
-            </FormGroup>
-          </form>
-        </FormControl>
-      </Grid>
+    return <Grid container justifyContent="center">
+        <Grid item xs={4}>
+            <form onSubmit={formik.handleSubmit}>
+                <FormControl>
+                    <FormLabel>
+                        <p>
+                            To log in get registered <a href={'https://social-network.samuraijs.com/'}
+                                                        target={'_blank'}>here</a>
+                        </p>
+                        <p>
+                            or use common test account credentials:
+                        </p>
+                        <p> Email: free@samuraijs.com
+                        </p>
+                        <p>
+                            Password: free
+                        </p>
+                    </FormLabel>
+                    <FormGroup>
+                        <TextField
+                            label="Email"
+                            margin="normal"
+                            {...formik.getFieldProps("email")}
+                        />
+                        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+                        <TextField
+                            type="password"
+                            label="Password"
+                            margin="normal"
+                            {...formik.getFieldProps("password")}
+                        />
+                        {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+                        <FormControlLabel
+                            label={'Remember me'}
+                            control={<Checkbox
+                                {...formik.getFieldProps("rememberMe")}
+                                checked={formik.values.rememberMe}
+                            />}
+                        />
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
+                    </FormGroup>
+                </FormControl>
+            </form>
+        </Grid>
     </Grid>
-  );
-};
+}
--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,98 +1,64 @@
-import { Dispatch } from "redux";
-import {
-  SetAppErrorActionType,
-  setAppIsInitialized,
-  SetAppIsInitializedType,
-  setAppStatusAC,
-  SetAppStatusActionType,
-} from "../../app/app-reducer";
-import { authAPI } from "../../api/todolists-api";
-import {
-  handleServerAppError,
-  handleServerNetworkError,
-} from "../../utils/error-utils";
-import { LoginDataType } from "./Login";
+import {Dispatch} from 'redux'
+import {SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../../app/app-reducer'
+import {authAPI, LoginParamsType} from '../../api/todolists-api'
+import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils'
 
-const initialState = {
-  isLoggedIn: false,
-};
-type InitialStateType = typeof initialState;
+const initialState: InitialStateType = {
+    isLoggedIn: false
+}
 
-export const authReducer = (
-  state: InitialStateType = initialState,
-  action: ActionsType
-): InitialStateType => {
-  switch (action.type) {
-    case "login/SET-IS-LOGGED-IN":
-      //debugger
-      return { ...state, isLoggedIn: action.value };
+export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'login/SET-IS-LOGGED-IN':
+            return {...state, isLoggedIn: action.value}
+        default:
+            return state
+    }
+}
 
-    default:
-      return state;
-  }
-};
 // actions
+
 export const setIsLoggedInAC = (value: boolean) =>
-  ({ type: "login/SET-IS-LOGGED-IN", value } as const);
+    ({type: 'login/SET-IS-LOGGED-IN', value} as const)
+
 
 // thunks
-
-export const meTC = () => async (dispatch: Dispatch<ActionsType>) => {
-  dispatch(setAppStatusAC("loading"));
-  try {
-    const response = await authAPI.me();
-    if (response.data.resultCode === 0) {
-      dispatch(setIsLoggedInAC(true));
-      dispatch(setAppStatusAC("succeeded"));
-    } else {
-      handleServerAppError(response.data, dispatch);
-    }
-  } catch (e) {
-    handleServerNetworkError(e as { message: string }, dispatch);
-  } finally {
-    dispatch(setAppIsInitialized(true)); //в любом случае убрать крутилку после после проверки куки
-  }
-};
-
-export const loginTC =
-  (data: LoginDataType) => async (dispatch: Dispatch<ActionsType>) => {
-    dispatch(setAppStatusAC("loading"));
-    //     authAPI.login(data)
-    //     .then ((response)=> {
-    // if (response.data.resultCode === 0) {
-    //     setIsLoggedInAC(true)
-    // }
-    try {
-      const response = await authAPI.login(data);
-      if (response.data.resultCode === 0) {
-        dispatch(setIsLoggedInAC(true));
-        dispatch(setAppStatusAC("succeeded"));
-      } else {
-        handleServerAppError(response.data, dispatch);
-      }
-    } catch (e) {
-      handleServerNetworkError(e as { message: string }, dispatch);
-    }
-  };
-
-export const logoutTC = () => async (dispatch: Dispatch<ActionsType>) => {
-  dispatch(setAppStatusAC("loading"));
-  try {
-    const response = await authAPI.logout();
-    if (response.data.resultCode === 0) {
-      dispatch(setIsLoggedInAC(false));
-      dispatch(setAppStatusAC("succeeded"));
-    } else {
-      handleServerAppError(response.data, dispatch);
-    }
-  } catch (e) {
-    handleServerNetworkError(e as { message: string }, dispatch);
-  }
-};
+export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.login(data)
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(true))
+                dispatch(setAppStatusAC('succeeded'))
+            } else {
+                handleServerAppError(res.data, dispatch)
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
+export const logoutTC = () => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.logout()
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(false))
+                dispatch(setAppStatusAC('succeeded'))
+            } else {
+                handleServerAppError(res.data, dispatch)
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
 
 // types
-type ActionsType =
-  | ReturnType<typeof setIsLoggedInAC>
-  | SetAppStatusActionType
-  | SetAppErrorActionType
-  | SetAppIsInitializedType;
+
+type ActionsType = ReturnType<typeof setIsLoggedInAC>
+type InitialStateType = {
+    isLoggedIn: boolean
+}
+
+type ThunkDispatch = Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>
