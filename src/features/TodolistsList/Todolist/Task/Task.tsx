1dd509beead34b76db9a59909f9418e235b2b839
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-11-23 17:17:12 +0300

    baseUrl

--- src/features/TodolistsList/Todolist/Task/Task.tsx
+++ src/features/TodolistsList/Todolist/Task/Task.tsx
@@ -1,8 +1,9 @@
 import React, { ChangeEvent, useCallback } from "react"
 import { Checkbox, IconButton } from "@mui/material"
-import { EditableSpan } from "../../../../components/EditableSpan/EditableSpan"
 import { Delete } from "@mui/icons-material"
-import { TaskStatuses, TaskType } from "../../../../api/todolists-api"
+import { TaskStatuses, TaskType } from "api/todolists-api"
+import { EditableSpan } from "components/EditableSpan/EditableSpan"
+
 
 type TaskPropsType = {
   task: TaskType
