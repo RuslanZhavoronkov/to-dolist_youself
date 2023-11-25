97b2e385e862fb3b89c7146b615c0128b011ebdd
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-11-25 20:29:43 +0300

    Lesson 17 - change app-reducer-test

--- src/app/app-reducer.test.ts
+++ src/app/app-reducer.test.ts
@@ -1,4 +1,13 @@
-import { appReducer, InitialStateType, setAppErrorAC, setAppStatusAC } from "./app-reducer"
+import { RequestStatusType, appActions, appReducer } from "./app-reducer"
+
+export type InitialStateType = {
+  // происходит ли сейчас взаимодействие с сервером
+  status: RequestStatusType
+  // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+  error: string | null
+  // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
+  isInitialized: boolean
+}
 
 let startState: InitialStateType
 
@@ -11,11 +20,13 @@ beforeEach(() => {
 })
 
 test("correct error message should be set", () => {
-  const endState = appReducer(startState, setAppErrorAC("some error"))
+  //const endState = appReducer(startState, setAppErrorAC("some error"))
+  const endState = appReducer(startState, appActions.setAppError({error:"some error"}))
   expect(endState.error).toBe("some error")
 })
 
 test("correct status should be set", () => {
-  const endState = appReducer(startState, setAppStatusAC("loading"))
+  //const endState = appReducer(startState, setAppStatusAC("loading"))
+  const endState = appReducer(startState, appActions.setAppStatus({status:"loading"}))
   expect(endState.status).toBe("loading")
 })
