25bab4080d3095f13416dd3547f4008adf2da8f5
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-12-15 23:40:33 +0300

    Lesson 20 - builder.addMatcher

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AnyAction, createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState = {
   status: "idle" as RequestStatusType,
@@ -16,13 +16,34 @@ const slice = createSlice({
     setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
       state.error = action.payload.error;
     },
-    setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
-      state.status = action.payload.status;
-    },
+    // setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+    //   state.status = action.payload.status;
+    // },
     setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
       state.isInitialized = action.payload.isInitialized;
     },
   },
+  extraReducers: (builder) => {
+    builder
+    .addMatcher((action: AnyAction)=>{
+      console.log('action matcher', action.type)
+      return action.type.endsWith('/pending')
+     }, (state, action)=>{
+      //debugger
+      console.log('action forReducer', action.type)
+      state.status = "loading"
+    })
+    .addMatcher((action: AnyAction)=> {
+      return action.type.endsWith('/fulfilled')
+    }, (state, action) => {
+      state.status = "succeeded"
+    })
+    .addMatcher((action: AnyAction)=> {
+      return action.type.endsWith('/rejected')
+    }, (state, action) => {
+    state.status = "failed"
+    })
+  }
 });
 
 export const appReducer = slice.reducer;
